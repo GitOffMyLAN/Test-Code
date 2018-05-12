fa4be9baf7651e3e7cf84c920ffdf642bc9d5232
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-05-12 17:51:33 +1000

    commitMessage

---
+++ Javascript/fizzbuzz.js
@@ -0,0 +1,8 @@
+output = ""
+limit = 100
+
+funciton fizzbuzz(testingNumber, Fizzing, Message, Output){
+	if(testingNumber %% Fizzing === 0){
+		output = output + testingNumber + ": " + + Message
+	}<++>
+}
