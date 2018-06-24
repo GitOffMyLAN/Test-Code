37a3ec3a2b33d185e95c3a08190822e1641ef2a1
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-24 11:13:07 +1000

    Added a basic get method for forms and stopped XSS in the strings. Also if the form is not fully filled out it gives and error

---
+++ PHP/Get and Post/getpost.php
@@ -0,0 +1,24 @@
+<?php
+	if($_GET['username'] != '' && $_GET['email'] != ''){
+		$setUsername = htmlentities($_GET['username']);
+		$setEmail = htmlentities($_GET['email']);
+		echo $setUsername;
+	} else {
+		echo "<script>alert('Please fill the entire form out plesase');</script>";
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP get and post test site</title>
+	</head>
+	<body>
+		<form action="getpost.php" method="GET">
+			<h1>Username</h1>
+			<input placeholder="username" name="username"></input>
+			<h1>Email</h1>
+			<input placeholder="e-mail" name="email"></input>
+			<button>Submit</button>
+		</form>
+	</body>
+</html>
