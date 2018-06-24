71f4d1a976551dbdf5cb96f135b2dec8a78a00b1
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-24 11:19:41 +1000

    made a POST method using PHP

--- PHP/Get and Post/getpost.php
+++ PHP/Get and Post/getpost.php
@@ -1,11 +1,20 @@
 <?php
+/*
 	if($_GET['username'] != '' && $_GET['email'] != ''){
 		$setUsername = htmlentities($_GET['username']);
 		$setEmail = htmlentities($_GET['email']);
 		echo $setUsername;
 	} else {
 		echo "<script>alert('Please fill the entire form out plesase');</script>";
+	}*/
+if($_POST['username'] != '' && $_POST['email'] != ''){
+		$setUsername = htmlentities($_POST['username']);
+		$setEmail = htmlentities($_POST['email']);
+		echo $setUsername;
+}	else {
+		echo "<script>alert('Please fill the entire form out plesase');</script>";
 	}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +22,7 @@
 		<title>PHP get and post test site</title>
 	</head>
 	<body>
-		<form action="getpost.php" method="GET">
+		<form action="getpost.php" method="POST">
 			<h1>Username</h1>
 			<input placeholder="username" name="username"></input>
 			<h1>Email</h1>
