a80e71e1222301346fb7dbb93faa247a4b87bd74
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-26 17:58:35 +1000

    added more to calculator

--- C++/calculator/calculator.cpp
+++ C++/calculator/calculator.cpp
@@ -3,9 +3,6 @@
 #include <iostream>
 using namespace std;
 
-float adding(float num1, float num2);
-float subtracting(float num1, float num2);
-
 //Adding Function
 float adding(float num1, float num2){
 	float result;
@@ -13,12 +10,27 @@ float adding(float num1, float num2){
 	return result;
 }
 
-//subtracting function
+//Subtracting function
 float subtracting(float num1, float num2){
 	float result;
 	result = num1 - num2;
 	return result;
 }
+
+//Times function
+float times(float num1, float num2){
+	float result;
+	result = num1 * num2;
+	return result;
+}
+
+//Dividing function
+float divid(float num1, float num2){
+	float result;
+	result = num1 / num2;
+	return result;
+}
+
 
 int main(){
 	float ret;
@@ -28,6 +40,13 @@ int main(){
 	num1 = 1;
 	num2 = 4;
 	ret = adding(num1, num2);
-	cout << "Adding: " << ret << endl;
+	cout << "+: " << ret << endl;
+	ret = subtracting(num1, num2);
+	cout << "-: " << ret << endl;
+	ret = times(num1, num2);
+	cout << "*: " << ret << endl;
+	ret = divid(num1, num2);
+	cout << "/: " << ret << endl;
+
 	return 0;
 }
